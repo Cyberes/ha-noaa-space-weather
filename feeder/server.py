7d55cde97b58d80a9eaaca935ac49a76b741115f
Cyberes <cyberes@evulid.cc>
2024-09-03 17:25:25 -0600

    cache image and fix service

--- feeder/server.py
+++ feeder/server.py
@@ -1,7 +1,8 @@
+import datetime
 import io
 
 import redis
-from flask import Flask, send_file
+from flask import Flask, send_file, make_response
 
 app = Flask(__name__)
 redis_client = redis.Redis(host='localhost', port=6379)
@@ -15,7 +16,12 @@ def serve_global_map():
 
     buf = io.BytesIO(global_map_data)
     buf.seek(0)
-    return send_file(buf, mimetype='image/png')
+    response = make_response(send_file(buf, mimetype='image/png'))
+    expires = datetime.datetime.now()
+    expires = expires + datetime.timedelta(minutes=10)
+    response.headers['Cache-Control'] = 'public'
+    response.headers['Expires'] = expires.strftime("%a, %d %b %Y %H:%M:%S GMT")
+    return response
 
 
 if __name__ == '__main__':
